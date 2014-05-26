06b26ef761141280086b09017b543a21d310433d
Katsunori SUZUI <kt@7kry.net>
2014-05-27 03:40:05 +0900

    Fix some error messages; Add `hourgraph'

--- tweets/__init__.py
+++ tweets/__init__.py
@@ -1,6 +1,11 @@
+import datetime
 import json
 import re
 import zipfile
+
+# dependencies
+import dateutil.tz
+import pytz
 
 def open(filename):
   with zipfile.ZipFile(filename) as fp:
@@ -9,3 +14,7 @@ def open(filename):
     for f in jslist:
       ret += reversed(json.loads(re.compile(r'.+?\n').sub("", fp.read(f).decode("ascii"), 1)))
     return ret
+
+def parse_created_at(created_at):
+  return datetime.datetime.strptime(created_at, "%Y-%m-%d %H:%M:%S +0000")\
+                 .replace(tzinfo = pytz.timezone("UTC")).astimezone(dateutil.tz.tzlocal())
