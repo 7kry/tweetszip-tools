91cf84c390d2ab176265dc2b967a6ddbd8f430c9
Katsunori SUZUI <kt@7kry.net>
2014-11-20 18:50:13 +0900

    Add tweetszip-merge

--- tweets/__init__.py
+++ tweets/__init__.py
@@ -7,6 +7,37 @@ import zipfile
 import dateutil.tz
 import pytz
 
+# Const
+CSV_SCHEMA = [
+  "tweet_id",
+  "in_reply_to_status_id",
+  "in_reply_to_user_id",
+  "timestamp",
+  "source",
+  "text",
+  "retweeted_status_id",
+  "retweeted_status_user_id",
+  "retweeted_status_timestamp",
+  "expanded_urls",
+]
+
+JSON_TSFORMAT = "%a %b %d %H:%M:%S %z %Y"
+CSV_TSFORMAT = "%Y-%m-%d %H:%M:%S %z"
+
+def status_to_csvrow(status):
+  return [
+    status['id'],                                                                     # tweet_id
+    status.get('in_reply_to_status_id', ''),                                          # in_reply_to_status_id
+    status.get('in_reply_to_user_id', ''),                                            # in_reply_to_user_id
+    status['created_at'],                                                             # timestamp
+    status['source'],                                                                 # source
+    status['text'],                                                                   # text
+    status['retweeted_status']['id'] if 'retweeted_status' in status else "",         # retweeted_status_id
+    status['retweeted_status']['user']['id'] if 'retweeted_status' in status else "", # retweeted_status_user_id_
+    status['retweeted_status']['created_at'] if 'retweeted_status' in status else "", # retweeted_status_timestamp
+    ",".join(map(lambda url: url['expanded_url'], status['entities']['urls'])),       # expanded_urls
+  ]
+
 def open(filename):
   with zipfile.ZipFile(filename) as fp:
     jslist = sorted(filter(lambda f: re.compile(r'^data/js/tweets/.+\.js$').match(f), fp.namelist()))
